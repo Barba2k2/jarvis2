8ae583c707d3c698692c0a917ddeb89788c4f3fb
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-05 14:37:46 -0300

    Inital Commit

---
+++ 001/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I love my girlfriend!!");
+        System.out.println("I really love my girlfriend!!");
+    }
+}
