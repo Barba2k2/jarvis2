4f6049f5e2cff3c30e0261171f264cee04534a2a
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-05 22:17:26 -0300

    RANDOM NUMBERS 🔢

---
+++ 008 - random numbers/Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Main {
+  public static void main(String[] args) {
+    Random random = new Random();
+
+    int number1;
+    double number2;
+    boolean isHeads;
+
+    number1 = random.nextInt(1, 101);
+    number2 = random.nextDouble();
+    isHeads = random.nextBoolean();
+
+    System.out.println(number1);
+    System.out.println(number2);
+
+    if(isHeads){
+      System.out.println("HEADS");
+    } else {
+      System.out.println("TAILS");
+    }
+  }
+}
